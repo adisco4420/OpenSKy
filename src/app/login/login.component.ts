3242733f12fd057831249cd72dc8ebde5b00d7ac
alabi sodiq <adisco4420@gmail.com>
2019-12-26 12:03:15 +0100

    login form validation and logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  errMsg = false;
+  loginForm = new FormGroup({
+    username: new FormControl('demo', Validators.required),
+    password: new FormControl('demo', Validators.required)
+  });
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    if (this.loginForm.valid) {
+      const { username, password} = this.loginForm.controls;
+      if (username.value === 'demo' && password.value === 'demo') {
+        this.errMsg = false;
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.errMsg = true;
+      }
+      console.log(this.loginForm.value);
+    }
   }
 
 }
