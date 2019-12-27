6066db4eda94b856870dad459ecb97889e97528f
alabi sodiq <adisco4420@gmail.com>
2019-12-27 02:51:13 +0100

    display list of airports

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { FligtModalComponent } from './../fligt-modal/fligt-modal.component';
+import { FlightService } from './../services/flight.service';
+import { AirportService } from './../services/airport.service';
+import { Component, OnInit, } from '@angular/core';
+import {MatDialog, } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  airportList = [];
+  arrivalList = [{}];
+  departureList = [{}];
+  dialogRef;
+  constructor(
+    private airportSrv: AirportService,
+    private flightSrv: FlightService,
+    public dialog: MatDialog) { }
 
   ngOnInit() {
+    this.getAirports();
+  }
+  getAirports() {
+    this.airportSrv.fetchAirports().subscribe(res => {
+     this.airportList = res;
+    });
+  }
+  open(airport) {
+    this.openDialog({
+      airport
+    });
+  }
+  openDialog(data) {
+    this.dialogRef = this.dialog.open(FligtModalComponent, {
+      data,
+      width: '480px'
+    });
   }
 
 }
